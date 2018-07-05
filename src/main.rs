a005d553272274ce63eedfe769204c888dddd50b
Oleksandr Smoktal <oleksandr.smoktal@halliburton.com>
2018-07-05 15:07:00 +0300

    Some fixes

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,37 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 use regex::Regex;
 use hyper::{Body, Request, Response, Server};
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
+
+#[derive(Debug, Clone)]
+enum OpType {
+    Plus,
+    Minus,
+    Div,
+    Mul
+}
+
+impl OpType {
+    pub fn compute(&self, first_number: f32, second_number: f32) -> Response<Body> {
+        debug!("Data: {:?}, {:?}, {:?}", self, first_number, second_number);
+
+        let response = OpResponse::create(
+            match self {
+                OpType::Plus => first_number + second_number,
+                OpType::Minus => first_number - second_number,
+                OpType::Div => first_number / second_number,
+                OpType::Mul => first_number * second_number,
+            }
+        );
+
+        Response::new(Body::from(response.into_json()))
+    }
+}
 
 trait IntoJson: serde::Serialize {
     fn into_json(&self) -> String {
@@ -27,23 +52,36 @@ trait IntoJson: serde::Serialize {
 
 #[derive(Serialize)]
 struct ErrorResponse {
-    status: i32,
+    status: i16,
     error_message: String,
-    error_code: u32
+    error_code: u16
+}
+
+impl ErrorResponse {
+    fn create(error_message: String, error_code: u16) -> Response<Body> {
+        Response::builder()
+            .status(error_code)
+            .body(Body::from(ErrorResponse {
+                status: -1,
+                error_message: error_message.into(),
+                error_code: error_code
+            }.into_json()))
+            .unwrap()
+    }
 }
 
 impl IntoJson for ErrorResponse {}
 
 #[derive(Serialize)]
 struct OpResponse {
-    status: i32,
+    status: i16,
     result: f32,
 }
 
-impl From<f32> for OpResponse {
-    fn from(fpoint: f32) -> Self {
+impl OpResponse {
+    fn create(fpoint: f32) -> Self {
         OpResponse {
-            status: 0i32,
+            status: 0,
             result: fpoint
         }
     }
@@ -51,73 +89,60 @@ impl From<f32> for OpResponse {
 
 impl IntoJson for OpResponse {}
 
-fn calculate(method: String, first_number: f32, second_number: f32) -> Response<Body> {
-    debug!("Data: {:?}, {:?}, {:?}", method, first_number, second_number);
+lazy_static! {
+    static ref METHODS_MAP: HashMap<&'static str, OpType> = {
+        let mut map = HashMap::new();
 
-    let response = OpResponse::from(
-        match method.as_str() {
-            "plus" => first_number + second_number,
-            "minus" => first_number - second_number,
-            "div" => first_number / second_number,
-            "mul" => first_number * second_number,
-            _ => unreachable!()
-        }
-    );
-    
-    Response::new(Body::from(response.into_json()))
+        map.insert("plus", OpType::Plus);
+        map.insert("minus", OpType::Minus);
+        map.insert("div", OpType::Div);
+        map.insert("mul", OpType::Mul);
+
+        map
+    };
 }
 
-fn error_response(error_message: &str, error_code: u32) -> Response<Body> {
-    Response::builder()
-        .status(error_code as u16)
-        .body(Body::from(ErrorResponse { 
-            status: -1i32,
-            error_message: error_message.into(),
-            error_code: error_code
-        }.into_json()))
-        .unwrap()
-}
-
-fn check_method(method: &str) -> Option<String> {
-    let methods_set: HashSet<String> = ["plus", "minus", "div", "mul"]
-        .iter()
-        .map(|s| s.to_string())
-        .collect();
+fn check_method(method: &str) -> Option<OpType> {
     let clean_method = method.to_lowercase();
 
-    if methods_set.contains(&clean_method) {
-        Some(clean_method)
-    } else {
-        None
-    }
+    METHODS_MAP.get(clean_method.as_str()).cloned()
 }
 
-fn check_number(number: &str) -> Option<f32> {
-    number.parse::<f32>().ok()
+fn check_number(number: &str) -> Result<f32, &str> {
+    let result = serde_json::from_str(number).map_err(|_e| "Invalid number format. Should be a JSON number")?;
+
+    if result < -1e9 {
+        Err("Number should not be less than -1e9")
+    } else if result > 1e9 {
+        Err("Number should not be greater than 1e9")
+    } else {
+        Ok(result)
+    }
 }
 
 fn router(req: Request<Body>) -> Response<Body> {
     lazy_static! {
-        static ref RE: Regex = Regex::new(r"^/([[:alpha:]]+)/([-+]?[0-9]*(\.[0-9]+([eE][-+]?[0-9]+)?)?)/([-+]?[0-9]*(\.[0-9]+([eE][-+]?[0-9]+)?)?)$").unwrap();
+        static ref RE: Regex = Regex::new(r"^/([[:alpha:]]+)/(-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?)/(-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?)$").unwrap();
     }
-    
+
     let caps = RE.captures(req.uri().path());
-    
+
     match caps {
         Some(args) => {
             match check_method(args.get(1).unwrap().as_str()) {
                 Some(method) => {
                     match (check_number(args.get(2).unwrap().as_str()),
-                           check_number(args.get(5).unwrap().as_str())) {
-                        (Some(first_num), Some(second_num)) => calculate(method, first_num, second_num),
-                        _ => error_response("Invalid arguments", 404)
+                           check_number(args.get(3).unwrap().as_str())) {
+                        (Ok(first_num), Ok(second_num)) => method.compute(first_num, second_num),
+                        (Err(first_num_err), _) => ErrorResponse::create(format!("Invalid first argument: {}", first_num_err), 405),
+                        (_, Err(second_num_err)) => ErrorResponse::create(format!("Invalid second argument: {}", second_num_err), 405),
                     }
                 },
-                _ => error_response("Invalid method", 404)
+                _ => ErrorResponse::create(format!("Invalid method. Possible methods: {:?}", METHODS_MAP.keys()), 405)
             }
         },
         _ => {
-            error_response("Malformed url", 404)
+            ErrorResponse::create("Invalid url".to_string(), 404)
         }
     }
 }
